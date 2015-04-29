deaf84786baf402aa16dc05694a339bc4a4ca63a
Isaías <isaiahlima18@gmail.com>
2015-04-29 08:21:24 -0300

    Profile

--- iProgress/ReposViewController.swift
+++ iProgress/ReposViewController.swift
@@ -1,56 +1,23 @@
 //
-//  ProfileViewController.swift
+//  ReposViewController.swift
 //  iProgress
 //
-//  Created by Isaías Lima on 28/04/15.
+//  Created by Isaías Lima on 29/04/15.
 //  Copyright (c) 2015 Eduardo Quadros. All rights reserved.
 //
 
 import UIKit
 
-class ProfileViewController: UITableViewController {
+class ReposViewController: UITableViewController {
 
-    
-    @IBOutlet var photo: UIImageView!
-    var avatar: UIImage!
-    var search: Search!
-    var userData: NSDictionary!
-    @IBOutlet var nick: UILabel!
-    @IBOutlet var followers: UILabel!
-    @IBOutlet var following: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        search = Search.sharedInstance
-        userData = search.userData
-        
-        var avatarURL = String()
-        avatarURL = userData.objectForKey("avatar_url") as! String
-        
-        var data = NSData(contentsOfURL: NSURL(string: avatarURL)!)
-        
-        avatar = UIImage(data: data!)
-        
-        photo.image = avatar
-        photo.layer.masksToBounds = true
-        photo.layer.cornerRadius = 20
-        
-        nick.text = userData.objectForKey("login") as? String
-        
-        var follow = userData.objectForKey("followers") as! Int
-        followers.text = "\(follow)"
-        
-        var followed = userData.objectForKey("following") as! Int
-        following.text = "\(followed)"
-    }
-    
-    @IBAction func github(sender: AnyObject) {
-        var user = userData.objectForKey("login") as! String
-        var url = "https://www.github.com/\(user)"
-        
-        UIApplication.sharedApplication().openURL(NSURL(string: url)!)
-        
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -60,29 +27,26 @@ class ProfileViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-/*
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return 5
     }
-*/
-
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("repos", forIndexPath: indexPath) as! UITableViewCell
 
-        // Configure the cell...
+        cell.textLabel?.text = "Xablau"
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
